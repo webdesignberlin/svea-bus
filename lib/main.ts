9ba371e4de4be6be4895f157b132699534604d66
Michael Gerstmann <michael.gerstmann@fitx.de>
2023-06-01 10:19:36 +0200

    simple event handling

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,32 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(++counter))
-  setCounter(0)
-}
+type EventHandler = (...args: any[]) => void;
+
+export const sveaBus = () => {
+  const eventHandlers: { [event: string]: EventHandler[] } = {};
+
+  const on = (event: string, handler: EventHandler) => {
+    if (!eventHandlers[event]) {
+      eventHandlers[event] = [];
+    }
+    eventHandlers[event].push(handler);
+  };
+
+  const emit = (event: string, ...args: any[]) => {
+    const handlers = eventHandlers[event];
+    if (handlers) {
+      handlers.forEach((handler) => handler(...args));
+    }
+  };
+
+  // Clean up event handlers on component unmount
+  const clear = () => {
+    for (const event in eventHandlers) {
+      delete eventHandlers[event];
+    }
+  };
+
+  return {
+    on,
+    emit,
+    clear,
+  };
+};
